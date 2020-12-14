71d84efad5e55214bac3a52d88c3e95910d349fd
chenjiinguyen <duyntp2000@gmail.com>
2020-12-14 23:19:02 +0700

    Init ASP MVC

---
+++ ThinkProWebsite/Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThinkProWebsite.Models;
+
+namespace ThinkProWebsite.Controllers
+{
+    public class MenuController : Controller
+    {
+        ThinkProDataContext db = new ThinkProDataContext();
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MenuPatial()
+        {
+            ViewBag.Loai = db.LOAIs.OrderBy(t => t.TENLOAI).ToList();
+            ViewBag.ThuongHieu = db.THUONGHIEUs.OrderBy(t=> t.TENTH).ToList();
+            return View();
+        }
+    }
+}
---
+++ ThinkProWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThinkProWebsite.MvcApplication" Language="C#" %>
