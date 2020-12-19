55417f4cea3f5d8c31b56f30a86ef1f096c40988
chenjiinguyen <duyntp2000@gmail.com>
2020-12-19 23:56:59 +0700

    Reconstruct Project

--- ThinkProWebsite/Controllers/HomeController.cs
+++ ThinkProWebsite/Controllers/HomeController.cs
@@ -3,28 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ThinkProWebsite.Models;
 
 namespace ThinkProWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        ThinkProDataContext db = new ThinkProDataContext();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Product(string id)
         {
-            ViewBag.Message = "Your application description page.";
+            var Product = db.SANPHAMs.SingleOrDefault(u => u.ID_SP == id);
+            if (Product != null)
+            {
+                ViewBag.ListReview = db.DANHGIAs.Where(t => t.ID_SP == id).OrderByDescending(t => t.NGAYGIO).ToList();
+                ViewBag.AvgReview = db.DANHGIAs.Where(t => t.ID_SP == id).Average(t => (int?)t.XEPHANG);
+                ViewBag.InfoProduct = db.THONGTINs.SingleOrDefault(t => t.ID_SP == id);
+                return View(Product);
 
-            return View();
+            }
+            return RedirectToAction("Index", "Home");
         }
 
-        public ActionResult Contact()
+        public ActionResult Products(string loai = "", string brand = "")
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.loai = loai;
+            ViewBag.brand = brand;
+            List<string> _ListBrand = (brand == "") ? new List<string>() : brand.Split(',').ToList();
 
-            return View();
+            var ListProduct = new List<SANPHAM>();
+            if (loai == "" && _ListBrand.Count == 0)
+            {
+                ListProduct = db.SANPHAMs.ToList();
+                ViewBag.Name = "Danh Sách Sản Phẩm";
+            }
+            else
+            {
+                if (loai != "")
+                {
+                    ListProduct = db.SANPHAMs.Where(t => t.ID_LOAI == loai).ToList();
+                    var Loai = db.LOAIs.Single(t => t.ID_LOAI == loai);
+                    ViewBag.Name = Loai.TENLOAI;
+                }
+                else if (_ListBrand.Count > 0)
+                {
+                    ListProduct = db.SANPHAMs.Where(t => _ListBrand.Contains(t.ID_BRAND)).ToList();
+                    ViewBag.Name = "Danh Sách Sản Phẩm";
+                }
+                else
+                {
+                    ListProduct = db.SANPHAMs.Where(t => t.ID_LOAI == loai && _ListBrand.Contains(t.ID_BRAND)).ToList();
+                    var Loai = db.LOAIs.Single(t => t.ID_LOAI == loai);
+                    ViewBag.Name = Loai.TENLOAI;
+                }
+            }
+
+
+
+            return View(ListProduct);
         }
     }
 }
---
+++ ThinkProWebsite/Controllers/PartialController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThinkProWebsite.Models;
+
+namespace ThinkProWebsite.Controllers
+{
+    public class PartialController : Controller
+    {
+        ThinkProDataContext db = new ThinkProDataContext();
+
+        public ActionResult MenuPartial()
+        {
+            ViewBag.Loai = db.LOAIs.OrderBy(t => t.TENLOAI).ToList();
+            ViewBag.ThuongHieu = db.THUONGHIEUs.OrderBy(t => t.TENTH).ToList();
+            return PartialView();
+        }
+
+        public ActionResult BrandFilterPartial(string idBrand = "", string idLoai = "")
+        {
+            List<string> _ListLoai = (idLoai == "") ? new List<string>() : idLoai.Split(',').ToList();
+            List<string> _ListBrand = (idBrand == "") ? new List<string>() : idBrand.Split(',').ToList();
+
+            var ListBrand = db.THUONGHIEUs.ToList();
+            List<int> CountBrand = new List<int>();
+            foreach (var Brand in ListBrand)
+            {
+
+                if (_ListLoai.Count == 0)
+                    CountBrand.Add(db.SANPHAMs.Where(t => Brand.ID_BRAND == t.ID_BRAND).Count());
+                else
+                    CountBrand.Add(db.SANPHAMs.Where(t => Brand.ID_BRAND == t.ID_BRAND && _ListLoai.Contains(t.ID_LOAI)).Count());
+            }
+            ViewBag.ListBrand = ListBrand;
+            ViewBag.CountBrand = CountBrand;
+            ViewBag._ListBrand = _ListBrand;
+            return PartialView();
+        }
+
+        public ActionResult LoaiFilterPartial(string idLoai = null, string idBrand = null)
+        {
+            List<string> _ListLoai = (idLoai == null) ? new List<string>() : idLoai.Split(',').ToList();
+            List<string> _ListBrand = (idBrand == null) ? new List<string>() : idBrand.Split(',').ToList();
+
+            var ListLoai = db.LOAIs.ToList();
+            List<int> CountLoai = new List<int>();
+            foreach (var Loai in ListLoai)
+            {
+                if (_ListBrand.Count == 0)
+                    CountLoai.Add(db.SANPHAMs.Where(t => t.ID_LOAI == Loai.ID_LOAI).Count());
+                else
+                    CountLoai.Add(db.SANPHAMs.Where(t => t.ID_LOAI == Loai.ID_LOAI && _ListBrand.Contains(t.ID_BRAND)).Count());
+            }
+
+            ViewBag.ListLoai = ListLoai;
+            ViewBag.CountLoai = CountLoai;
+            ViewBag._ListLoai = _ListLoai;
+            return PartialView();
+        }
+    }
+}
