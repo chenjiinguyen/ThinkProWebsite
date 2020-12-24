6aeb22383aba3a9aab49a3484629764fc242e229
chenjiinguyen <duyntp2000@gmail.com>
2020-12-24 12:41:01 +0700

    Revert "Merge branch 'master' into Cart"
    This reverts commit 76a61f9f9ea4b5f72dc684ec91775a5ab4561f3d, reversing
    changes made to 6a139a27443b02c442bdc698b1bbd2e8045e1281.

--- ThinkProWebsite/Controllers/HomeController.cs
+++ ThinkProWebsite/Controllers/HomeController.cs
@@ -3,114 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ThinkProWebsite.Models;
 
 namespace ThinkProWebsite.Controllers
 {
     public class HomeController : Controller
     {
-        ThinkProDataContext db = new ThinkProDataContext();
-        private static Random random = new Random();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Product(string id)
+        public ActionResult Index1()
         {
-            var Product = db.SANPHAMs.SingleOrDefault(u => u.ID_SP == id);
-            if (Product != null)
-            {
-                ViewBag.ListReview = db.DANHGIAs.Where(t => t.ID_SP == id).OrderByDescending(t => t.NGAYGIO).ToList();
-                ViewBag.AvgReview = db.DANHGIAs.Where(t => t.ID_SP == id).Average(t => (int?)t.XEPHANG);
-                ViewBag.InfoProduct = db.THONGTINs.SingleOrDefault(t => t.ID_SP == id);
-                return View(Product);
-
-            }
-            return RedirectToAction("Index", "Home");
+            return View();
         }
 
-        public ActionResult Products(string loai = "", string brand = "", int paging = 1)
+        public ActionResult About()
         {
-            int PageSize = 12;
-            ViewBag.loai = loai;
-            ViewBag.brand = brand;
-            List<string> _ListBrand = (brand == "") ? new List<string>() : brand.Split(',').ToList();
+            ViewBag.Message = "Your application description page.";
 
-
-            var ListProduct = new List<SANPHAM>();
-            if (loai == "" && _ListBrand.Count == 0)
-            {
-                ListProduct = db.SANPHAMs.ToList();
-                ViewBag.Name = "Danh Sách Sản Phẩm";
-            }
-            else
-            {
-                if (loai != "" && _ListBrand.Count == 0)
-                {
-                    ListProduct = db.SANPHAMs.Where(t => t.ID_LOAI == loai).ToList();
-                    var Loai = db.LOAIs.Single(t => t.ID_LOAI == loai);
-                    ViewBag.Name = Loai.TENLOAI;
-                }
-                else if (_ListBrand.Count > 0 && loai == "")
-                {
-                    ListProduct = db.SANPHAMs.Where(t =>  _ListBrand.Contains(t.ID_BRAND)).ToList();
-                    ViewBag.Name = "Danh Sách Sản Phẩm";
-                }
-                else
-                {
-                    ListProduct = db.SANPHAMs.Where(t => t.ID_LOAI == loai && _ListBrand.Contains(t.ID_BRAND)).ToList();
-                    var Loai = db.LOAIs.Single(t => t.ID_LOAI == loai);
-                    ViewBag.Name = Loai.TENLOAI;
-                }
-            }
-            int count = ListProduct.Count;
-            ViewBag.MaxPage = Math.Ceiling((double)count / PageSize);
-            ViewBag.Page = paging;
-            ListProduct = ListProduct.Skip((paging - 1) * PageSize).Take(PageSize).ToList();
-            return View(ListProduct);
+            return View();
         }
 
-        public ActionResult Review(string id)
+        public ActionResult Contact()
         {
-            var Product = db.SANPHAMs.SingleOrDefault(u => u.ID_SP == id);
-            if (Product != null)
-            {
-                return View(Product);
+            ViewBag.Message = "Your contact page.";
 
-            }
-            return RedirectToAction("Index", "Home");
-        }
-
-        [HttpPost]
-        public ActionResult Review(string product, int danhgia, string review_tieude, string review_noidung)
-        {
-            var Product = db.SANPHAMs.SingleOrDefault(u => u.ID_SP == product);
-            if (Product != null)
-            {
-                DANHGIA Review = new DANHGIA();
-                Review.ID_DG = RandomString(8);
-                Review.HOTEN = "Anonymous";
-                Review.ID_SP = product;
-                Review.NGAYGIO = DateTime.Now;
-                Review.TIEUDE = review_tieude;
-                Review.NOIDUNG = review_noidung;
-                Review.XEPHANG = danhgia;
-                db.DANHGIAs.InsertOnSubmit(Review);
-                db.SubmitChanges();
-                return RedirectToAction("Product", "Home", new { id = product });
-            }
-            return RedirectToAction("Index", "Home");
-        }
-
-
-
-
-        public string RandomString(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, length)
-              .Select(s => s[random.Next(s.Length)]).ToArray());
+            return View();
         }
     }
 }
---
+++ ThinkProWebsite/Controllers/LoaiController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThinkProWebsite.Models;
+
+namespace ThinkProWebsite.Controllers
+{
+    public class LoaiController : Controller
+    {
+        ThinkProDataContext db = new ThinkProDataContext();
+        // GET: Loai
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoaiFilterPatial(string idLoai = null,string idBrand = null)
+        {
+            List<string> _ListLoai = (idLoai == null)? new List<string>(): idLoai.Split(',').ToList();
+            List<string> _ListBrand = (idBrand == null) ? new List<string>() : idBrand.Split(',').ToList();
+
+            var ListLoai = db.LOAIs.ToList();
+            List<int> CountLoai = new List<int>();
+            foreach (var Loai in ListLoai)
+            {
+                if(_ListBrand.Count == 0)
+                    CountLoai.Add(db.SANPHAMs.Where(t => t.ID_LOAI == Loai.ID_LOAI).Count());
+                else
+                    CountLoai.Add(db.SANPHAMs.Where(t => t.ID_LOAI == Loai.ID_LOAI && _ListBrand.Contains(t.ID_BRAND)).Count());
+            }
+
+            ViewBag.ListLoai = ListLoai;
+            ViewBag.CountLoai = CountLoai;
+            ViewBag._ListLoai = _ListLoai;
+            return View();
+        }
+    }
+}
